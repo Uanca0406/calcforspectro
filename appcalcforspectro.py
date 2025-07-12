2fd8f3d71b4e49a1c8cdd292fd09252508605a60
Uanca0406 <ff1359238@gmail.com>
2025-07-12 08:53:00 +0700

    Update appcalcforspectro.py

--- appcalcforspectro.py
+++ appcalcforspectro.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(
-    np.random.randn(10, 5), columns=("col %d" % i for i in range(5))
+    np.random.randn(10, 2), columns=("col %d" % i for i in range(2))
 )
 
 st.table(df)
